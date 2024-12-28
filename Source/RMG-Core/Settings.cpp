148c81a92f310151310da3f0cb28bed4d10cac9a
Nayla Hanegan <naylahanegan@gmail.com>
2024-12-28 18:43:56 -0500

    update netplay username and server url

--- Source/RMG-Core/Settings.cpp
+++ Source/RMG-Core/Settings.cpp
@@ -160,10 +160,10 @@ static l_Setting get_setting(SettingsID settingId)
         break;
 
     case SettingsID::Netplay_Nickname:
-        setting = {SETTING_SECTION_NETPLAY, "Nickname", "NetplayUser"};
+        setting = {SETTING_SECTION_NETPLAY, "Nickname", "MPN Player"};
         break;
     case SettingsID::Netplay_ServerJsonUrl:
-        setting = {SETTING_SECTION_NETPLAY, "ServerJsonUrl", "https://m64p.s3.amazonaws.com/servers.json"};
+        setting = {SETTING_SECTION_NETPLAY, "ServerJsonUrl", "https://mariopartynetplay.netlify.app/servers.json"};
         break;
 
     case SettingsID::Core_GFX_Plugin:
