ba95aea1f68f4263794194ac648fce06b30e91c6
Nayla Hanegan <naylahanegan@gmail.com>
2024-12-28 17:45:01 -0500

    add art, mpn cheats

--- Source/RMG/UserInterface/Dialog/AboutDialog.cpp
+++ Source/RMG/UserInterface/Dialog/AboutDialog.cpp
@@ -8,17 +8,14 @@
  *  along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
 #include "AboutDialog.hpp"
-
-#include <RMG-Core/Version.hpp>
+#include <RMG-Core/VersionHash.hpp>
+#include <RMG-Core/Core.hpp>
 
 using namespace UserInterface::Dialog;
 
 AboutDialog::AboutDialog(QWidget* parent) : QDialog(parent)
 {
     this->setupUi(this);
-    // prepare modified html w/ version
-    QString html = this->textBrowser->toHtml();
-    html.replace("{version}", QString::fromStdString(CoreGetVersion()));
-    // show modified html
-    this->textBrowser->setHtml(html);
+
+    this->versionLabel->setText(QString("%1").arg(QString::fromStdString(VERSION_HASH)));
 }
