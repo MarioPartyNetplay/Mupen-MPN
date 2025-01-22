1056e0ba393cbfdadbbd61f79fc23d508da7ffc5
Nayla Hanegan <naylahanegan@gmail.com>
2025-01-22 17:39:16 -0500

    der

--- Source/RMG/UserInterface/Dialog/Netplay/CreateNetplaySessionDialog.cpp
+++ Source/RMG/UserInterface/Dialog/Netplay/CreateNetplaySessionDialog.cpp
@@ -118,7 +118,6 @@ QString CreateNetplaySessionDialog::getGameName(QString goodName, QString file)
 bool CreateNetplaySessionDialog::validate(void)
 {
     if (this->nickNameLineEdit->text().isEmpty() ||
-        this->nickNameLineEdit->text().contains(' ') ||
         this->nickNameLineEdit->text().size() > 128)
     {
         return false;
