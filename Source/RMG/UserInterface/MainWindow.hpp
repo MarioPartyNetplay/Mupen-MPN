ae8ac66448c620763ba17d02c04d7b6a41ab6532
Nayla Hanegan <naylahanegan@gmail.com>
2025-01-24 17:49:11 -0500

    der

--- Source/RMG/UserInterface/MainWindow.hpp
+++ Source/RMG/UserInterface/MainWindow.hpp
@@ -237,7 +237,6 @@ class MainWindow : public QMainWindow, private Ui::MainWindow
     void on_RomBrowser_Cheats(QString file);
 
     void on_Netplay_PlayGame(QString file, QString address, int port, int player, QJsonArray cheats);
-    void on_NetplaySessionBrowser_rejected(void);
     void on_NetplaySessionDialog_rejected(void);
 
   public slots:
