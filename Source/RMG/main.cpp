36867fcaff8920e0ec2b0dd026c9802e318ce775
Tabitha Hanegan <naylahanegan@gmail.com>
2025-03-15 19:14:12 -0400

    mu

--- Source/RMG/main.cpp
+++ Source/RMG/main.cpp
@@ -192,7 +192,7 @@ int main(int argc, char **argv)
     }
 #endif
 
-    QCoreApplication::setApplicationName("Rosalie's Mupen GUI");
+    QCoreApplication::setApplicationName("Mupen MPN");
     QCoreApplication::setApplicationVersion(QString::fromStdString(CoreGetVersion()));
 
     // setup commandline parser
