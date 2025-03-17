2bc8ae0f14c7cdb07e1fc055c5512d2385166ddb
Tabitha Hanegan <naylahanegan@gmail.com>
2025-03-17 12:52:04 -0400

    update loop fix

--- Source/RMG/UserInterface/Dialog/Update/UpdateDialog.cpp
+++ Source/RMG/UserInterface/Dialog/Update/UpdateDialog.cpp
@@ -41,7 +41,7 @@ UpdateDialog::UpdateDialog(QWidget *parent, QJsonObject jsonObject, bool forced)
 
     // Create and set up the text edit
     textEdit = new QTextEdit(this);
-    #ifdef __APPLE__ or _WIN32
+    #if defined(__APPLE__) || defined(_WIN32)
     textEdit->setText(jsonObject.value(QStringLiteral("body")).toString());
     #else
     textEdit->setText(QStringLiteral("Auto Updater is not supported on your platform.\nPlease use your package manager to update."));
