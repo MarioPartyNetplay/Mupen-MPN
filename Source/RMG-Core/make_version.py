f967d87c08a32ddaf8db7132aa1891a670db1f02
Tabitha <naylahanegan@gmail.com>
2025-03-17 13:19:52 -0400

    Update make_version.py

--- Source/RMG-Core/make_version.py
+++ Source/RMG-Core/make_version.py
@@ -1,25 +1,29 @@
 #!/usr/bin/env python3
-import sys
 import os
 import subprocess
+
 def system(cmd, default):
-    args = cmd.split()
     try:
-        return subprocess.check_output(args).decode("ascii").strip()
+        return subprocess.check_output(cmd.split()).decode("ascii").strip()
     except subprocess.CalledProcessError:
         return default
+
 if __name__ == "__main__":
-    hash = system("git show -s --format=%h", "0" * 40)
+    hash = system("git rev-parse --short=7 HEAD", "0" * 7)  # Ensuring a 7-character fallback
+
     mappings = {
         "GIT_COMMIT_HASH": hash,
     }
+
     base_path = os.path.dirname(os.path.abspath(__file__))
-    core_path = os.path.join(base_path)
-    in_path = os.path.join(core_path, "VersionHash.hpp.in")
-    out_path = os.path.join(core_path, "VersionHash.hpp")
+    in_path = os.path.join(base_path, "VersionHash.hpp.in")
+    out_path = os.path.join(base_path, "VersionHash.hpp")
+
     with open(in_path, "r") as f:
         version_str = f.read()
+
     for mapping, value in mappings.items():
         version_str = version_str.replace("@" + mapping + "@", value)
+
     with open(out_path, "w") as f:
         f.write(version_str)
