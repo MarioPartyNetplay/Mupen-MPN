b5d8e1234326e75717bb7468abb21951b7abaac6
Tabitha Hanegan <naylahanegan@gmail.com>
2025-04-09 16:57:07 -0400

    ge

--- Source/3rdParty/mupen64plus-core/src/main/netplay.c
+++ Source/3rdParty/mupen64plus-core/src/main/netplay.c
@@ -722,7 +722,7 @@
  }
 
 // Add this function to get the current emulation speed
-uint8_t netplay_get_emulation_speed(char void)
+uint8_t netplay_get_emulation_speed(void)
 {
     return l_emulation_speed;
 }
